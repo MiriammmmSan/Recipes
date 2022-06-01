3b6d5814a676574f27f58692c7a8282932fd81f0
Masha <masha@MacBook-Pro-Rusanov.local>
2022-06-01 12:49:36 +0400

    recipes card

--- src/components/RecipesCard.js
+++ src/components/RecipesCard.js
@@ -1,9 +1,14 @@
 import CustomImg from "./Customimg"
 
-export default function RecipesCard(){
+export default function RecipesCard({recipe}){
     return (
         <div className="recipe-card">
-            <CustomImg imgSrc="/img/img1.jpeg" pt="60%"/>
+            <CustomImg imgSrc={recipe.img} pt="60%"/>
+            <div className="recipe-card-info">
+                <p className="recipe-title">{recipe.title}</p>
+                <p className="recipe-text">{recipe.preview}</p>
+                <a className="view-btn" href="#!">VIEW RECIPE</a>
+            </div>
         </div>
     )
 }
